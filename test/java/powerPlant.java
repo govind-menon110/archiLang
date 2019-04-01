66454ec43b2eb80d2db32c80fe50af4464efbda7
Sotirios Katsikeas <skatsikeas@outlook.com>
2019-04-01 14:05:02 +0200

    More work done on instantiated model

--- test/java/powerPlant.java
+++ test/java/powerPlant.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import org.apache.commons.lang3.Validate;
 import org.junit.After;
 
 import powerGrid.*;
@@ -7,39 +8,72 @@ public class powerPlant {
     
    @Test
    public void overallCycleTestModel() {
-       // Instantiate all the basic assets
+       // Instantiate some of the basic assets here
        TransmissionGrid grid = new TransmissionGrid("TransmissionGrid");
        GridTransmissionSwitch gridSwitch = new GridTransmissionSwitch("PowerGridSwitch");
        PowerPlant plant = new PowerPlant("PowerPlant");
        FeedWaterTank fwaterTank = new FeedWaterTank("FeedWaterTank");
-       Heater heater = new Heater("Heater");
-       Cooler cooler = new Cooler("Cooler");
-       Turbine turbine1 = new Turbine("Turbine1");
-       Generator generator1 = new Generator("Generator1");
-       RotatingEquipment rotEquipment = new RotatingEquipment("RotatingEquipment");
+       //RotatingEquipment rotEquipment = new RotatingEquipment("RotatingEquipment");
        Fuel fuel = new Fuel("Fuel");
-       Coolant coolant = new Coolant("Coolant");
-       // Connect them with each other
+       // Then connect them with each other
        gridSwitch.addTransmissionGrid(grid);
        plant.addTransmissionGrid(grid);
        plant.addFeedWaterTank(fwaterTank);
-       plant.addCooler(cooler);
-       plant.addHeater(heater);
+       // Steam is condensed in the cooler by the coolant. The condensate is then pumped to the feedwater tank
+       Coolant coolant = new Coolant("Coolant");
+       Valve coolantValve = new Valve("CoolantValve");
+       Cooler cooler = new Cooler("Cooler");
+       coolant.addValveCoolant(coolantValve);
        cooler.addCoolantCooler(coolant);
-       heater.addHeaterFuel(fuel);
+       plant.addCooler(cooler);
+       // QUESTION: How is Cooler connected with PumpCompressor (AuxilaryRotatingEquipment)???
+       PumpCompressor coolerPump1 = new PumpCompressor("CoolerPump1");
+       PumpCompressor coolerPump2 = new PumpCompressor("CoolerPump2");
+       // QUESTION: How is coolerPump1 and coolerPump2 connected with coolerValve???
 
+       Valve coolerValve = new Valve("CoolerValve");
+       cooler.addValveCooler(coolerValve);
+       // From the tank the redundant feedwater pumps pump the water in the heater.
+       PumpCompressor feedWaterPump1 = new PumpCompressor("FeedWaterPump1");
+       PumpCompressor feedWaterPump2 = new PumpCompressor("FeedWaterPump2");
+       PumpCompressor feedWaterPump3 = new PumpCompressor("FeedWaterPump3");
+       Valve waterPumpValve1 = new Valve("WaterPumpValve1");
+       Valve waterPumpValve2 = new Valve("WaterPumpValve2");
+       Valve waterPumpValve3 = new Valve("WaterPumpValve3");
+       // Heater (to High pressure turbine)
+       Heater heater1 = new Heater("Heater1");
+       heater1.addHeaterFuel(fuel);
+       plant.addHeater(heater1);
+       Valve heaterValve1 = Valve("HeaterValve1");
+       Valve heaterValve2 = Valve("HeaterValve2");
+       // Heater2 (to intermediate pressure turbine)
+       Heater heater2 = new Heater("Heater2");
+       heater2.addHeaterFuel(fuel);
+       plant.addHeater(heater2);
+       Valve heater2Valve1 = Valve("Heater2Valve1");
+       Valve heater2Valve2 = Valve("Heater2Valve2");
+       // Turbines
+       Turbine highPressTurbine = new Turbine("HighPressureTurbine");
+       Turbine IntermediatePressTurbine = new Turbine("IntermediatePressureTurbine");
+       Turbine lowPressTurbine = new Turbine("LowPressureTurbine");
+       // QUESTION: How are turbines connected to the generator? How is then the generator connected to the transmission grid?
+       // All turbines are connected to the generator by a solid shaft
+       Generator generator = new Generator("Generator");
+       // Hydraulic Control System
        Controller hydrSysController = new Controller("HydraulicSysController");
        HydraulicControlSystem hydrCtrlSystem = new HydraulicControlSystem("HydraulicCtrlSys");
        hydrCtrlSystem.addConroller(hydrSysController);
        // Add Valves to HydraulicControlSystem
-       Valve pumpValve1 = new Valve("PumpValve1");
-       Valve pumpValve2 = new Valve("PumpValve2");
-       Valve pumpValve3 = new Valve("PumpValve3");
-       hydrCtrlSystem.addValve(pumpValve1);
-       hydrCtrlSystem.addValve(pumpValve2);
-       hydrCtrlSystem.addValve(pumpValve3);
-       // Not complete, TODO: complete with all connected Valves
-
+       hydrCtrlSystem.addValve(waterPumpValve1);
+       hydrCtrlSystem.addValve(waterPumpValve2);
+       hydrCtrlSystem.addValve(waterPumpValve3);
+       hydrCtrlSystem.addValve(heaterValve1);
+       hydrCtrlSystem.addValve(heaterValve2);
+       hydrCtrlSystem.addValve(heater2Valve1);
+       hydrCtrlSystem.addValve(heater2Valve2);
+       hydrCtrlSystem.addValve(coolantValve);
+       hydrCtrlSystem.addValve(coolerValve);
+       
        Attacker attacker = new Attacker();
        // TODO Entry point
        //attacker.addAttackPoint(TODO);
