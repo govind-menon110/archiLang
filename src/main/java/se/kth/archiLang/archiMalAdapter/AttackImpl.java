147a895fed35972d037c1d2e7a1587cbe628c2a8
Simon <simon.hacks@swc.rwth-aachen.de>
2019-04-02 09:34:19 +0200

    Right reference for follow upp attacks fixed.

--- src/main/java/se/kth/archiLang/archiMalAdapter/AttackImpl.java
+++ src/main/java/se/kth/archiLang/archiMalAdapter/AttackImpl.java
@@ -152,7 +152,7 @@ public class AttackImpl implements Attack {
         StringBuilder qualifiedName = new StringBuilder();
 
         if (extension) {
-            qualifiedName.append(MetaElements.format(elementContainer.getNameOfElement(getRelatedClass())) + ".");
+            qualifiedName.append(WordUtils.uncapitalize(MetaElements.format(elementContainer.getNameOfElement(getRelatedClass()))) + ".");
         }
 
         qualifiedName.append(getName());
